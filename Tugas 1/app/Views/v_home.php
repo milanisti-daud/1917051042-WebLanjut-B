752bd1e7be40687137f8a72d57f93b9318606e10
milanisti-daud <milanisti.daud@gmail.com>
2021-09-17 01:10:49 +0700

    first command

---
+++ Tugas 1/app/Views/v_home.php
@@ -0,0 +1,5 @@
+
+<div class="container">
+    <h1>Ini halaman home</h1>
+</div>
+    
---
+++ Tugas 1/app/Views/v_posts.php
@@ -0,0 +1,6 @@
+<div class="container mt-3">
+<div class="text-center">
+    <h1>All Posts</h1>
+</div>
+</div>
+    
